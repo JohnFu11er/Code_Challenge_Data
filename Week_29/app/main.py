2d3ef9807055654967a32d6fa004de2002361296
John Fuller <john.adrift@gmail.com>
2021-06-11 01:01:47 -0400

    updated configs

--- Week_29/app/main.py
+++ Week_29/app/main.py
@@ -1,5 +1,36 @@
+import requests
 from fastapi import FastAPI
-from my_functions import basic_auth, device_list
+from requests.auth import HTTPBasicAuth
+
+
+def basic_auth(username, password):    
+
+    api_URL = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
+
+    data = requests.post(
+        api_URL,
+        auth=HTTPBasicAuth(username, password)
+    ).json()
+
+    return data
+
+
+def device_list(username, password):
+    api_auth = basic_auth(username, password)
+    
+    headers = {
+    'X-Auth-Token': api_auth['Token'],
+    'content-type': 'application/json'
+    }
+
+    api_URL = "http://sandboxdnac.cisco.com/dna/intent/api/v1/network-device"
+    data = requests.get(
+        api_URL,
+        headers= headers
+    ).json()
+    
+    return data
+
 
 app = FastAPI()
 
